87c849adda8063d4ce9acdc266f2b076d1d60c51
Ian Pilipski <ian@pilipski.com>
2021-03-14 16:31:18 -0400

    updating versions file

--- updateversion.sh
+++ updateversion.sh
@@ -7,11 +7,13 @@ set -e
 [ -e ./.tmp_nsub ] && rm -rf ./.tmp_nsub
 nuget install NSubstitute -Version $VERSION -Framework netstandard2.0 -OutputDirectory ./.tmp_nsub
 
-[ -e Packages/IPTech.NSubstitute/Editor/NSubstitute ] && rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute
-[ -e Packages/IPTech.NSubstitute/Editor/CastleCore ] && rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore
+[ -e Packages/IPTech.NSubstitute/Editor/NSubstitute ] && find Packages/IPTech.NSubstitute/Editor/NSubstitute -type f -not -name *.meta -delete
+[ -e Packages/IPTech.NSubstitute/Editor/CastleCore ] && find Packages/IPTech.NSubstitute/Editor/CastleCore -type f -not -name *.meta -delete
+[ -e Packages/IPTech.NSubstitute/Editor/ThreadingExtensions ] && find Packages/IPTech.NSubstitute/Editor/ThreadingExtensions -type f -not -name *.meta -delete
 
-cp -r ./.tmp_nsub/NSubstitute.$VERSION Packages/IPTech.NSubstitute/Editor/NSubstitute
-cp -r ./.tmp_nsub/Castle.Core.4.0.0 Packages/IPTech.NSubstitute/Editor/CastleCore
+cp -r ./.tmp_nsub/NSubstitute.$VERSION/ Packages/IPTech.NSubstitute/Editor/NSubstitute
+cp -r ./.tmp_nsub/Castle.Core.4.0.0/ Packages/IPTech.NSubstitute/Editor/CastleCore
+cp -r ./.tmp_nsub/System.Threading.Tasks.Extensions.4.0.0/ Packages/IPTech.NSubstitute/Editor/ThreadingExtensions
 
 rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/lib/net35
 rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/lib/net40
@@ -25,3 +27,6 @@ rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/lib/net45
 rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/*.nupkg
 rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/.signature.p7s
 
+rm -rf Packages/IPTech.NSubstitute/Editor/ThreadingExtensions/lib/portable*
+rm -rf Packages/IPTech.NSubstitute/Editor/ThreadingExtensions/*.nupkg
+rm -rf Packages/IPTech.NSubstitute/Editor/ThreadingExtensions/.signature.p7s
