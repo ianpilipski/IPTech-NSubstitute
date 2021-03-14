237ac81ff5a7be22f6f5fe5a1dd1b6b0242230ad
Ian Pilipski <ian@pilipski.com>
2021-03-14 14:01:28 -0400

    Moved files under Packages folder

--- updateversion.sh
+++ updateversion.sh
@@ -7,21 +7,21 @@ set -e
 [ -e ./.tmp_nsub ] && rm -rf ./.tmp_nsub
 nuget install NSubstitute -Version $VERSION -Framework netstandard2.0 -OutputDirectory ./.tmp_nsub
 
-[ -e Assets/IPTech.NSubstitute/NSubstitute ] && rm -rf Assets/IPTech.NSubstitute/NSubstitute
-[ -e Assets/IPTech.NSubstitute/CastleCore ] && rm -rf Assets/IPTech.NSubstitute/CastleCore
+[ -e Packages/IPTech.NSubstitute/Editor/NSubstitute ] && rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute
+[ -e Packages/IPTech.NSubstitute/Editor/CastleCore ] && rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore
 
-cp -r ./.tmp_nsub/NSubstitute.$VERSION Assets/IPTech.NSubstitute/NSubstitute
-cp -r ./.tmp_nsub/Castle.Core.4.0.0 Assets/IPTech.NSubstitute/CastleCore
+cp -r ./.tmp_nsub/NSubstitute.$VERSION Packages/IPTech.NSubstitute/Editor/NSubstitute
+cp -r ./.tmp_nsub/Castle.Core.4.0.0 Packages/IPTech.NSubstitute/Editor/CastleCore
 
-rm -rf Assets/IPTech.NSubstitute/NSubstitute/lib/net35
-rm -rf Assets/IPTech.NSubstitute/NSubstitute/lib/net40
-rm -rf Assets/IPTech.NSubstitute/NSubstitute/lib/net45
-rm -rf Assets/IPTech.NSubstitute/NSubstitute/*.nupkg
-rm -rf Assets/IPTech.NSubstitute/NSubstitute/.signature.p7s
+rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/lib/net35
+rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/lib/net40
+rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/lib/net45
+rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/*.nupkg
+rm -rf Packages/IPTech.NSubstitute/Editor/NSubstitute/.signature.p7s
 
-rm -rf Assets/IPTech.NSubstitute/CastleCore/lib/net35
-rm -rf Assets/IPTech.NSubstitute/CastleCore/lib/net40-client
-rm -rf Assets/IPTech.NSubstitute/CastleCore/lib/net45
-rm -rf Assets/IPTech.NSubstitute/CastleCore/*.nupkg
-rm -rf Assets/IPTech.NSubstitute/CastleCore/.signature.p7s
+rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/lib/net35
+rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/lib/net40-client
+rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/lib/net45
+rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/*.nupkg
+rm -rf Packages/IPTech.NSubstitute/Editor/CastleCore/.signature.p7s
 
